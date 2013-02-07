3dead0f358da5c1976894d75ef3b2662c268639f
Lane Kolbly <lane@rscheme.org>
2013-02-07 17:12:22 -0600

    Added handle_JavaWithRunner function.

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ from twisted.internet import reactor
 from twisted.web.server import Site, resource
 from twisted.web.static import File
 from twisted.python import log
-import cgi, sys, os, subprocess
+import cgi, sys, os, subprocess, shutil
 
 log.startLogging(sys.stdout)
-
-class Root(resource.Resource):
-    isLeaf = False
 
 import re, StringIO, random, hashlib
 
@@ -20,21 +17,37 @@ def call_command(cmd):
         return (False, output)
     return (True, output)
 
-# This class holds the concept of a "problem" that students are solving by
-# uploading all of these files.
-class Problem:
-    def run_on_testcase(self, testcase):
-        pass
+def handle_JavaWithRunner(runner_name):
+    output = ""
+    did_run = True
+    try:
+        output += subprocess.check_output(["javac", "%s.java"%runner_name], stderr=subprocess.STDOUT)
+        output += subprocess.check_output(["java", "%s"%runner_name], stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        output += e.output
+        did_run = False
+    return (did_run, output)
 
-    def run(self):
-        return call_command("java HelloWorld")
+def run_program(directory, username, problem_id, filename):
+    # Normally, the problems are defined in a DB (mongo, MySQL, etc.)
+    problems = {"Song": {"type": "JavaWithRunner", "runner_name": "SongRunner"}}
 
-def run_program(directory, filename):
+    # Go deal with some file system stuff...
     filename, extension = (filename.split(".")[0], filename.split(".")[1])
     os.chdir("root")
     os.chdir(directory)
     print "Entering directory %s..."%directory
 
+    output = ""
+    if problems[problem_id]["type"] == "JavaWithRunner":
+        shutil.copyfile("../../data/runners/%s.java"%problems[problem_id]["runner_name"], "./%s.java"%problems[problem_id]["runner_name"])
+        result = handle_JavaWithRunner(problems[problem_id]["runner_name"])
+        output = result[1]
+
+    os.chdir("../..")
+    return output
+
+def run_program_old():
     # If it's a zip file, unzip it...
     output = ""
     if extension == "zip":
@@ -52,20 +65,41 @@ def run_program(directory, filename):
                     break
 
         if not has_error:
-            p = Problem()
-            output += p.run()[1]
+            # Copy over the runner file
+            shutil("../../data/runners/SongRunner.java", ".")
+            output += call_command("java SongRunner.java")
+            pass
 
         pass
     else:
+        result = handle_JavaWithRunner()
+        output += result[1]
+        """
         output = ""
         try:
-            output += subprocess.check_output(["javac", "%s.java"%filename], stderr=subprocess.STDOUT)
-            output += subprocess.check_output(["java", "%s"%filename], stderr=subprocess.STDOUT)
+            output += subprocess.check_output(["javac", "SongRunner.java"], stderr=subprocess.STDOUT)
+            output += subprocess.check_output(["java", "SongRunner"], stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
             output += e.output
+            """
 
     os.chdir("../..")
     return output
+
+class Root(resource.Resource):
+    isLeaf = False
+    def getChild(self, name, request):
+        print "Name: %s"%name
+        if name == "index.html" or name == "":
+            return self
+        elif name == "upload":
+            return UploadView()
+        elif name == "static":
+            return File("./static")
+        return self
+
+    def render_GET(self, request):
+        return open("./static/index.html").read()
 
 class UploadView(resource.Resource):
     isLeaf = True
@@ -85,15 +119,23 @@ class UploadView(resource.Resource):
         out.write(request.args["upl_file"][0])
         out.close()
 
+        username = request.args["username"][0]
+        problem_id = request.args["problem"][0]
+
         # Run it (or queue it for running, or whatever)
         #output = subprocess.check_output(["javac", "%s.java"%filename])
         #output += subprocess.check_output(["java", "%s"%filename])
-        output = run_program(d, filename+"."+extension)
+        output = run_program(d, username, problem_id, filename+"."+extension)
+
+        # Comb output to make it nicer for HTML-formatted output
+        output = output.replace("<", "&lt;")
+        output = output.replace(">", "&gt;")
+        output = output.replace("\n", "<br/>")
+        output = "<html><body>%s<br/><a href=\"/\">Try again</a></body></html>"%output
+
         return output
 
 root = Root()
-root.putChild("upload", UploadView())
-root.putChild("static", File("static/"))
 site = Site(root)
 reactor.listenTCP(8005, site)
 reactor.run()
